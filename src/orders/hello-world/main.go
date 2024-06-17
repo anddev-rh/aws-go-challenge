22b1d0b88f15f95054c02a8bd8f5be32fc862a11
Andres R Hernandez <andresrh222@gmail.com>
2024-06-17 17:24:17 -0500

    Handle request in orders, with validation

--- src/orders/hello-world/main.go
+++ src/orders/hello-world/main.go
@@ -1,24 +1,68 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/uuid"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var greeting string
-	sourceIP := request.RequestContext.Identity.SourceIP
+type CreateOrderRequest struct {
+	UserID     string `json:"user_id"`
+	Item       string `json:"item"`
+	Quantity   int    `json:"quantity"`
+	TotalPrice int64  `json:"total_price"`
+}
 
-	if sourceIP == "" {
-		greeting = "Hello, world!\n"
-	} else {
-		greeting = fmt.Sprintf("Hello, %s!\n", sourceIP)
+type CreateOrderEvent struct {
+	OrderID    string `json:"order_id"`
+	TotalPrice int64  `json:"total_price"`
+}
+
+func validateCreateOrderRequest(body []byte) (*CreateOrderRequest, error) {
+	var createOrderRequest CreateOrderRequest
+	err := json.Unmarshal(body, &createOrderRequest)
+	if err != nil {
+		return nil, err
 	}
 
+	if createOrderRequest.UserID == "" {
+		return nil, errors.New("invalid user_id")
+	}
+	if createOrderRequest.Item == "" {
+		return nil, errors.New("invalid item")
+	}
+	if createOrderRequest.Quantity <= 0 {
+		return nil, errors.New("invalid quantity")
+	}
+	if createOrderRequest.TotalPrice <= 0 {
+		return nil, errors.New("invalid total_price")
+	}
+
+	return &createOrderRequest, nil
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	validatedRequest, err := validateCreateOrderRequest([]byte(request.Body))
+	fmt.Println("err:", err, "validatedRequest:", validatedRequest)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       fmt.Sprintf("Invalid request body: %v", err),
+		}, nil
+	}
+
+	myEvent := CreateOrderEvent{
+		OrderID:    uuid.New().String(),
+		TotalPrice: validatedRequest.TotalPrice,
+	}
+
+	myEventBytes, _ := json.Marshal(myEvent)
 	return events.APIGatewayProxyResponse{
-		Body:       greeting,
+		Body:       string(myEventBytes),
 		StatusCode: 200,
 	}, nil
 }
