9c5ef727aa047c1d4556399a265c8e956fb4ab0f
Andres R Hernandez <andresrh222@gmail.com>
2024-06-19 13:48:46 -0500

    [FTR] Add dynamoDB UTIL (#12)

---
+++ src/microservices/go.mod
@@ -0,0 +1,3 @@
+module utils/dynamodb.go
+
+go 1.22.4
---
+++ src/microservices/utils/dynamodb_util.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+var dynamoDBClient *dynamodb.DynamoDB
+
+func initDynamoDBClient() {
+	sess := session.Must(session.NewSession())
+	dynamoDBClient = dynamodb.New(sess)
+}
+
+func SaveToDynamoDB(tableName, orderID string, item interface{}) error {
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		return err
+	}
+
+	if dynamoDBClient == nil {
+		sess := session.Must(session.NewSession())
+		dynamoDBClient = dynamodb.New(sess)
+	}
+
+	av["order_id"] = &dynamodb.AttributeValue{
+		S: aws.String(orderID),
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = dynamoDBClient.PutItem(input)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
